426a2d8f82f71786bc025a332b9229dca23a49d0
Алексей Диденко <alex.didenko753@gmail.com>
2017-10-25 22:18:53 +0300

    Форма входа. Тесты работают

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 SampleApp3::Application.routes.draw do
 
-  resources :users
-
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
 
   root  'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :users
+  #match '/signup',  to: 'users#new',            via: 'get'
+  #match '/help',    to: 'static_pages#help',    via: 'get'
+  #match '/about',   to: 'static_pages#about',   via: 'get'
+  #match '/contact', to: 'static_pages#contact', via: 'get'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
