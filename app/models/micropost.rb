d5f801e1cabf86e275c33fcdd0757236ce32efef
Алексей Диденко <alex.didenko753@gmail.com>
2017-11-04 14:51:35 +0300

    добавил pictires

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,15 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order('created_at DESC') }
+  mount_uploader :picture, PictureUploader
+  validate  :picture_size
+
+  private
+
+  # Проводит валидацию размера загруженного изображения.
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
